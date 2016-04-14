0e346af3150338415fa62992518b6b6782e1a030
自由自在的小鱼 <hbbcs@sina.com>
2016-04-14 10:26:40 +0800

    修改样式

--- theme/views/header.php
+++ theme/views/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
 
 
     <link rel="stylesheet" type="text/css" media="screen" href="<?= THEMEPATH ?>/css/bootstrap.min.css"/>
@@ -29,43 +29,33 @@
 </head>
 <body>
 <!-- Header -->
-<div class="navbar navbar-inverse navbar-fixed-top header-back">
-    <div class="navbar-header">
-        <a class="navbar-brand" href="#"><img src="<?= THEMEPATH ?>/images/index.png" width="20px"/></a>
+<nav class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?=SITEPATH?>">
+                <img src="<?= THEMEPATH ?>/images/index.png" width="20px"/>
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+            <ul class="nav navbar-nav">
+                <li><a href="<?= SITEPATH ?>">首页</a></li>
+                <li><a href="<?= SITEPATH ?>/admin">后台</a></li>
+                <?php foreach ($categorys as $c): ?>
+                    <li class='<?= isset($id) && ($id == $c['id']) ? 'active' : '' ?>'>
+                        <a href="<?= SITEPATH ?>/category/<?= $c['id'] ?>"><?= $c['title'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </div>
-    <div>
-        <ul class="nav navbar-nav">
-            <li><a href="<?=SITEPATH?>">首页</a></li>
-        </ul>
+</nav>
 
-        <ul class="nav navbar-nav navbar-right">
-            <?php if ($user) { ?>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                        <img src="<?= THEMEPATH ?>/images/avater/<$user['avater']>" alt="..." width="20px" class="img-circle">
-                        <?= $user['name'] ?>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#" data-toggle="modal" data-target="#loginModal" data-whatever='{"title":"修改密码","url":"<?=SITEPATH?>/users/modify"}'><i class="glyphicon glyphicon-pencil"></i> 修改密码</a>
-                        </li>
-                        <li><a href="#"><i class="glyphicon glyphicon-wrench"></i> 我的属性</a></li>
-                        <li><a href="#"><i class="glyphicon glyphicon-inbox"></i> 我的消息</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="<?=SITEPATH?>/logout"><i class="glyphicon glyphicon-off"></i> 退出登陆</a></li>
-                    </ul>
-                </li>
-
-            <?php } else { ?>
-                <li><a href='<?=SITEPATH?>/login'>登陆</a></li>
-                <li><a href='<?=SITEPATH?>/register'>注册</a></li>
-            <?php } ?>
-            <li><a href="#"></a></li>
-        </ul>
-
-    </div>
-
-</div>
 <!-- End Header -->
 
 
--- theme/views/main.php
+++ theme/views/main.php
@@ -1,81 +1,45 @@
 <?php include VIEWPATH . "header.php" ?>
 
-<div class="row">
+<div class="col-md-2 pull-right">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">最新更新</h3>
+        </div>
 
-    <div class="col-md-10">
-
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="<?=SITEPATH?>">分类</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <?php foreach ($categorys as $c): ?>
-                            <li class='<?= isset($id) && ($id == $c['id']) ? 'active' : '' ?>'>
-                                <a href="<?= SITEPATH ?>/category/<?= $c['id'] ?>"><?= $c['title'] ?></a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container-fluid -->
-        </nav>
-
-        <div id="story_list">
-            <?php foreach ($story as $s): ?>
-                <div class="story-list">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>">
-                                <img src="<?=SITEPATH?>/<?= $s['image'] ? $s['image'] : 'books/default.jpg' ?>" width="160px" height="200px" title="<?= $s['desc'] ?>"/>
-                            </a>
-
-                            <p>
-                                <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>">
-                                    <b><?= $s['title'] ?></b>
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <table class="table">
+            <?php foreach ($update as $u): ?>
+                <tr>
+                    <td>
+                        <a href="<?= SITEPATH ?>/story/<?= $u['story_id'] ?>"><?= $u['story_title'] ?></a>
+                    </td>
+                    <td class="text-right"><?= $u['time'] ?></td>
+                </tr>
             <?php endforeach; ?>
-        </div>
+        </table>
 
     </div>
-
-    <div class="col-md-2">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">最新更新</h3>
-            </div>
-
-            <table class="table">
-                <?php foreach ($update as $u): ?>
-                    <tr>
-                        <td>
-                            <a href="<?= SITEPATH ?>/story/<?= $u['story_id'] ?>"><?= $u['story_title'] ?></a>
-                        </td>
-                        <td class="text-right"><?= $u['time'] ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-
-        </div>
-    </div>
-
 </div>
 
+<div class="col-md-10">
+    <div id="story_list">
+        <?php foreach ($story as $s): ?>
+            <div class="story-list pull-left">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>">
+                            <img src="<?= SITEPATH ?>/<?= $s['image'] ? $s['image'] : 'books/default.jpg' ?>" width="160px" height="200px" title="<?= $s['desc'] ?>"/>
+                        </a>
+
+                        <p>
+                            <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>">
+                                <b><?= $s['title'] ?></b>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
 
 <?php include VIEWPATH . "footer.php" ?>
