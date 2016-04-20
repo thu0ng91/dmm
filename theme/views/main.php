7e31cea1ba93a57e25700a5fd856f6278fb99908
自由自在的小鱼 <hbbcs@sina.com>
2016-04-20 21:23:43 +0800

    Update main.php

--- theme/views/main.php
+++ theme/views/main.php
@@ -3,24 +3,34 @@
     <div class="panel" >
         <div class="panel-body">
             <div class="slideshow">
-                <ul class="slide-con">
-                    <?php foreach ($update as $s): ?>
-
-                        <li>
-                            <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>" title="<?= $s['title'] ?>" target="_blank" class="pic">
-                                <img src="<?= SITEPATH ?>/<?= $s['image'] ?>" alt="<?= $s['title'] ?>" data-toggle="tooltip" data-placement="right" title="<?=$s['desc']?>"/>
-                                <h3><?= $s['title'] ?></h3>
-                            </a>                            
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
                 <ul class="slide-tab">
                     <?php foreach ($update as $s): ?>
                         <li>
                             <span></span>
-                            <img src="<?= SITEPATH ?>/<?= $s['image'] ?>" alt="<?= $s['title'] ?>"/>
+                            <img src="<?= SITEPATH ?>/<?= $s['image'] ? $s['image'] : 'books/default.jpg' ?>" alt="<?= $s['title'] ?>"/>
                         </li>
                     <?php endforeach; ?>            
+                </ul>
+                <ul class="slide-con">
+                    <?php foreach ($update as $s): ?>
+
+                        <li>
+                            <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>" title="<?= $s['title'] ?>" target="_blank" >
+                                <img src="<?= SITEPATH ?>/<?= $s['image'] ? $s['image'] : 'books/default.jpg' ?>" class="pic" />                                
+                            </a>     
+                            <a href="<?= SITEPATH ?>/story/<?= $s['id'] ?>" title="<?= $s['title'] ?>" target="_blank" >
+                                <h3><?= $s['title'] ?></h3>
+                            </a> 
+                            <div class="text-right">
+                                <label>
+                                    作者：<?= $s['author'] ?>
+                                </label>
+                            </div>
+                            <div class="desc">
+                                <?= $s['desc'] ?>
+                            </div>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
         </div>
@@ -31,8 +41,8 @@
     $(function () {
         $(".slideshow .slide-con li:eq(0)").show();
         $(".slideshow .slide-tab li:eq(0)").addClass("on");
-        focusSwitch('.slideshow', '.slide-con', '.slide-tab', 2500);
-        $('[data-toggle="tooltip"]').tooltip({html:true,container: 'body'});
+        focusSwitch('.slideshow', '.slide-con', '.slide-tab', 5000);
+        $('[data-toggle="tooltip"]').tooltip({html: true, container: 'body'});
         //$("img.lazy").lazyload();
     });
 </script>
