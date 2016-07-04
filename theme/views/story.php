65799d494e926e55a2b8fd156da376f9806ac9ca
自由自在的小鱼 <hbbcs@sina.com>
2016-07-04 10:47:01 +0800

    保存最后阅读章节

--- theme/views/story.php
+++ theme/views/story.php
@@ -15,7 +15,7 @@
             </p>
 
             <?php if(isset($last_read['id'])):?>
-                <div class="pull-right">您最后阅读章节： <span id="last_read"><a href="<?SITEPATH?>/chapter/<?=$last_read['id']?>"><?=$last_read['title']?></a> </span></div>
+                <div class="pull-right">您最后阅读章节： <span id="last_read"><a href="<?=SITEPATH?>/chapter/<?=$last_read['id']?>"><?=$last_read['title']?></a> </span></div>
             <?php endif;?>
         </div>
 
